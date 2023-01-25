0099c3dbed4bb9d58d29cada4f02cbffb0cd0ecc
ardatoprak <ardattoprak@gmail.com>
2023-01-25 21:04:35 +0300

    jListDemo class added

---
+++ SwingPractice/src/main/java/com/mycompany/swingpractice/SwingPractice.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.swingpractice;
+
+/**
+ *
+ * @author ardatoprak
+ */
+public class SwingPractice {
+
+    public static void main(String[] args) {
+       System.out.println("Hello");
+    }
+}
